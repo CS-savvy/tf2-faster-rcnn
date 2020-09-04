42f546f62c4a1c782a38fccb2379f35fa35f3da4
mukul <mukul.kr99@gmail.com>
2020-09-05 01:49:45 +0530

    rpn

--- network/frcnn_resnet.py
+++ network/frcnn_resnet.py
@@ -1,5 +1,7 @@
-from tensorflow.keras.layers import Conv2D, Activation, Add, BatchNormalization, ZeroPadding2D, Input, MaxPool2D
+from tensorflow.keras.layers import Conv2D, Activation, Add, BatchNormalization, ZeroPadding2D,\
+    Input, MaxPool2D, TimeDistributed, Flatten, Dense, AvgPool2D
 from tensorflow.keras import backend as K
+from network.roi_pooling import RoiPoolingConv
 
 
 def identity_block(input_tensor, kernel_size, filters, stage, block, trainable=True):
@@ -87,3 +89,98 @@ def nn_base(input_tensor=None, input_shape = (None, None, 3), trainable=False):
 
     return x
 
+def conv_block_td(input_tensor, kernel_size, filters, stage, block, input_shape, strides=(2, 2), trainable=True):
+
+    # conv block time distributed
+
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    if K.image_dim_ordering() == 'tf':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = TimeDistributed(Conv2D(nb_filter1, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'), input_shape=input_shape, name=conv_name_base + '2a')(input_tensor)
+    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter2, (kernel_size, kernel_size), padding='same', trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '2b')(x)
+    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter3, (1, 1), kernel_initializer='normal'), name=conv_name_base + '2c', trainable=trainable)(x)
+    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
+
+    shortcut = TimeDistributed(Conv2D(nb_filter3, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '1')(input_tensor)
+    shortcut = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '1')(shortcut)
+
+    x = Add()([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+def identity_block_td(input_tensor, kernel_size, filters, stage, block, trainable=True):
+
+    # identity block time distributed
+
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    if K.image_dim_ordering() == 'tf':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = TimeDistributed(Conv2D(nb_filter1, (1, 1), trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '2a')(input_tensor)
+    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter2, (kernel_size, kernel_size), trainable=trainable, kernel_initializer='normal',padding='same'), name=conv_name_base + '2b')(x)
+    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter3, (1, 1), trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '2c')(x)
+    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
+
+    x = Add()([x, input_tensor])
+    x = Activation('relu')(x)
+
+    return x
+
+def classifier_layers(x, input_shape, trainable=False):
+
+    # compile times on theano tend to be very high, so we use smaller ROI pooling regions to workaround
+    # (hence a smaller stride in the region that follows the ROI pool)
+    if K.backend() == 'tensorflow':
+        x = conv_block_td(x, 3, [512, 512, 2048], stage=5, block='a', input_shape=input_shape, strides=(2, 2), trainable=trainable)
+    elif K.backend() == 'theano':
+        x = conv_block_td(x, 3, [512, 512, 2048], stage=5, block='a', input_shape=input_shape, strides=(1, 1), trainable=trainable)
+
+    x = identity_block_td(x, 3, [512, 512, 2048], stage=5, block='b', trainable=trainable)
+    x = identity_block_td(x, 3, [512, 512, 2048], stage=5, block='c', trainable=trainable)
+    x = TimeDistributed(AvgPool2D((7, 7)), name='avg_pool')(x)
+
+    return x
+
+def classifier(base_layers, input_rois, num_rois, nb_classes = 21, trainable=False):
+
+    # compile times on theano tend to be very high, so we use smaller ROI pooling regions to workaround
+
+    if K.backend() == 'tensorflow':
+        pooling_regions = 14
+        input_shape = (num_rois, 14, 14, 1024)
+    elif K.backend() == 'theano':
+        pooling_regions = 7
+        input_shape = (num_rois, 1024, 7, 7)
+
+    out_roi_pool = RoiPoolingConv(pooling_regions, num_rois)([base_layers, input_rois])
+    out = classifier_layers(out_roi_pool, input_shape=input_shape, trainable=True)
+
+    out = TimeDistributed(Flatten())(out)
+
+    out_class = TimeDistributed(Dense(nb_classes, activation='softmax', kernel_initializer='zero'), name='dense_class_{}'.format(nb_classes))(out)
+    # note: no regression target for bg class
+    out_regr = TimeDistributed(Dense(4 * (nb_classes-1), activation='linear', kernel_initializer='zero'), name='dense_regress_{}'.format(nb_classes))(out)
+    return [out_class, out_regr]
